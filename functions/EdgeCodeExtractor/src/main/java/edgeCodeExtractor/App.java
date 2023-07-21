2b0aacde45d3a8806cbe709954680588112305d0
szipan <szipan@outlook.com>
2023-07-21 07:41:13 +0000

    Let user specify the database name and table name from athena.

--- functions/EdgeCodeExtractor/src/main/java/edgeCodeExtractor/App.java
+++ functions/EdgeCodeExtractor/src/main/java/edgeCodeExtractor/App.java
@@ -9,14 +9,20 @@ import software.amazon.awssdk.services.athena.model.QueryExecutionContext;
 import software.amazon.awssdk.services.athena.model.ResultConfiguration;
 import software.amazon.awssdk.services.athena.model.StartQueryExecutionRequest;
 import software.amazon.awssdk.services.athena.model.StartQueryExecutionResponse;
+import software.amazon.awssdk.utils.StringUtils;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class App implements RequestHandler<Map<String, String>, String> {
         public String handleRequest(final Map<String, String> input, final Context context) {
-                String query = "SELECT distinct(location) FROM cloudfront_logs;";
-                String database = "default";
+                String database = input.get("athena_database_name");
+                if (StringUtils.isEmpty(database))
+                        database = "default";
+                String tableName = input.get("athena_table_name");
+                if (StringUtils.isEmpty(tableName))
+                        tableName = "cloudfront_logs";
+                String query = "SELECT distinct(location) FROM " + tableName + ";";
                 String outputLocation = "s3://" + input.get("edge_location_bucket");
                 String region = input.get("region");
 
